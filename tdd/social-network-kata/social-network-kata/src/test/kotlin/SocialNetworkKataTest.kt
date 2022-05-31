306e6f7ba0f33629af7b6d17dddae3c07130a45d
Kevin Denver <kevin.denver@gmail.com>
2022-05-31 12:54:50 +0100

    Initial commit

---
+++ tdd/social-network-kata/social-network-kata/src/test/kotlin/SocialNetworkKataTest.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isFalse
+
+class SocialNetworkKataTest {
+
+    @Test
+    fun `hello world`() {
+        expectThat(true).isFalse()
+    }
+}
