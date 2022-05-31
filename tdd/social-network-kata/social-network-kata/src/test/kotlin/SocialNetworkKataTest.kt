200cd173c637d4ac4f282d62dd3015e9ff5d07f1
Kevin Denver <kevin.denver@gmail.com>
2022-05-31 13:20:33 +0100

    Following: Charlie can subscribe to Alice’s and Bob’s timelines, and view an aggregated list of all subscriptions

--- tdd/social-network-kata/social-network-kata/src/test/kotlin/SocialNetworkKataTest.kt
+++ tdd/social-network-kata/social-network-kata/src/test/kotlin/SocialNetworkKataTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.containsExactly
+import strikt.assertions.map
 import java.util.*
 
 class SocialNetworkKataTest {
@@ -11,11 +12,12 @@ class SocialNetworkKataTest {
             .post(Message("I am cooking breakfast!"))
 
         expectThat(alice.timeline.allMessages())
+            .map(TimelineMessage::message)
             .containsExactly(Message("I am cooking breakfast!"))
     }
 
     @Test
-    fun `Reading - Bob can view Alice timeline`() {
+    fun `Reading - Bob can view Alice's timeline`() {
         val alice = User(Username("Alice"), MutableTimeline())
             .post(Message("I am cooking breakfast!"))
 
@@ -24,7 +26,27 @@ class SocialNetworkKataTest {
         val timeline = bob.viewTimelineFrom(alice)
 
         expectThat(timeline.allMessages())
+            .map(TimelineMessage::message)
             .containsExactly(Message("I am cooking breakfast!"))
+    }
+
+    @Test
+    fun `Following - Charlie can subscribe to Alice's and Bob's timelines, and view an aggregated list of all subscriptions`() {
+        val alice = User(Username("Alice"), MutableTimeline())
+            .post(Message("I am cooking breakfast!"))
+
+        val bob = User(Username("Bob"), MutableTimeline())
+            .post(Message("I am cooking dinner!"))
+
+        val charlie = User(Username("Charlie"), MutableTimeline())
+
+        val timeline = charlie.subscribeTo(alice, bob)
+
+        expectThat(timeline.allMessages())
+            .containsExactly(
+                TimelineMessage(alice, Message("I am cooking breakfast!")),
+                TimelineMessage(bob, Message("I am cooking dinner!")),
+            )
     }
 }
 
@@ -38,21 +60,26 @@ value class Message(val value: String) {
     }
 }
 
+data class TimelineMessage(
+    val user: User,
+    val message: Message
+)
+
 interface Timeline {
-    fun allMessages(): List<Message>
+    fun allMessages(): List<TimelineMessage>
 }
 
 class MutableTimeline : Timeline {
-    private val messages: LinkedList<Message> = LinkedList()
+    private val messages = LinkedList<TimelineMessage>()
 
-    fun post(message: Message) {
+    fun post(message: TimelineMessage) {
         messages.addFirst(message)
     }
 
     override fun allMessages() = messages.toList()
 }
 
-data class ImmutableTimeline(val message: List<Message>) : Timeline {
+data class ImmutableTimeline(val message: List<TimelineMessage>) : Timeline {
     override fun allMessages() = message
 }
 
@@ -61,8 +88,13 @@ data class User(
     val timeline: MutableTimeline
 ) {
     fun post(message: Message) = apply {
-        timeline.post(message)
+        timeline.post(TimelineMessage(this, message))
     }
 
     fun viewTimelineFrom(other: User) = ImmutableTimeline(other.timeline.allMessages())
+
+    fun subscribeTo(vararg users: User) = users
+        .map(User::timeline)
+        .flatMap(MutableTimeline::allMessages)
+        .let(::ImmutableTimeline)
 }
