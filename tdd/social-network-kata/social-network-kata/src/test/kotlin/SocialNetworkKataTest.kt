ec9e01bfddf276e5aa694216ded20d5e32165d4a
Kevin Denver <kevin.denver@gmail.com>
2022-05-31 13:04:59 +0100

    Posting: Alice can publish messages to a personal timeline

--- tdd/social-network-kata/social-network-kata/src/test/kotlin/SocialNetworkKataTest.kt
+++ tdd/social-network-kata/social-network-kata/src/test/kotlin/SocialNetworkKataTest.kt
@@ -1,11 +1,45 @@
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
-import strikt.assertions.isFalse
+import strikt.assertions.containsExactly
+import java.util.*
 
 class SocialNetworkKataTest {
 
     @Test
-    fun `hello world`() {
-        expectThat(true).isFalse()
+    fun `Posting - Alice can publish messages to a personal timeline`() {
+        val alice = User(Username("Alice"), MutableTimeline())
+            .post(Message("I am cooking breakfast!"))
+
+        expectThat(alice.timeline.allMessages())
+            .containsExactly(Message("I am cooking breakfast!"))
+    }
+}
+
+@JvmInline
+value class Username(val value: String)
+
+@JvmInline
+value class Message(val value: String) {
+    init {
+        require(value.isNotBlank())
+    }
+}
+
+class MutableTimeline {
+    private val messages: LinkedList<Message> = LinkedList()
+
+    fun post(message: Message) {
+        messages.addFirst(message)
+    }
+
+    fun allMessages() = messages.toList()
+}
+
+data class User(
+    val username: Username,
+    val timeline: MutableTimeline
+) {
+    fun post(message: Message) = apply {
+        timeline.post(message)
     }
 }
