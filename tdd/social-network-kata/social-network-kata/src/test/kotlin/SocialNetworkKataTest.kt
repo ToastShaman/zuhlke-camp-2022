c429135c1fb1ec5ab4d0e04258b0a4fe6be44ec7
Kevin Denver <kevin.denver@gmail.com>
2022-05-31 13:46:35 +0100

    Mentions: Bob can link to Charlie in a message using “@”

--- tdd/social-network-kata/social-network-kata/src/test/kotlin/SocialNetworkKataTest.kt
+++ tdd/social-network-kata/social-network-kata/src/test/kotlin/SocialNetworkKataTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.containsExactly
+import strikt.assertions.first
 import strikt.assertions.map
 import java.util.*
 
@@ -8,7 +9,9 @@ class SocialNetworkKataTest {
 
     @Test
     fun `Posting - Alice can publish messages to a personal timeline`() {
-        val alice = User(Username("Alice"), MutableTimeline())
+        val network = TheSocialNetwork()
+
+        val alice = network.createUser(Username("Alice"))
             .post(Message("I am cooking breakfast!"))
 
         expectThat(alice.timeline.allMessages())
@@ -18,10 +21,12 @@ class SocialNetworkKataTest {
 
     @Test
     fun `Reading - Bob can view Alice's timeline`() {
-        val alice = User(Username("Alice"), MutableTimeline())
+        val network = TheSocialNetwork()
+
+        val alice = network.createUser(Username("Alice"))
             .post(Message("I am cooking breakfast!"))
 
-        val bob = User(Username("Bob"), MutableTimeline())
+        val bob = network.createUser(Username("Bob"))
 
         val timeline = bob.viewTimelineFrom(alice)
 
@@ -32,13 +37,15 @@ class SocialNetworkKataTest {
 
     @Test
     fun `Following - Charlie can subscribe to Alice's and Bob's timelines, and view an aggregated list of all subscriptions`() {
-        val alice = User(Username("Alice"), MutableTimeline())
+        val network = TheSocialNetwork()
+
+        val alice = network.createUser(Username("Alice"))
             .post(Message("I am cooking breakfast!"))
 
-        val bob = User(Username("Bob"), MutableTimeline())
+        val bob = network.createUser(Username("Bob"))
             .post(Message("I am cooking dinner!"))
 
-        val charlie = User(Username("Charlie"), MutableTimeline())
+        val charlie = network.createUser(Username("Charlie"))
 
         val timeline = charlie.subscribeTo(alice, bob)
 
@@ -48,6 +55,37 @@ class SocialNetworkKataTest {
                 TimelineMessage(bob, Message("I am cooking dinner!")),
             )
     }
+
+    @Test
+    fun `Mentions - Bob can link to Charlie in a message using @`() {
+        val network = TheSocialNetwork()
+
+        val charlie = network.createUser(Username("Charlie"))
+
+        val bob = network.createUser(Username("Bob"))
+            .post(Message("I am cooking dinner! Come over @Charlie"))
+
+        expectThat(charlie.viewTimelineFrom(bob))
+            .get { message }
+            .first()
+            .get { mentions }
+            .containsExactly(charlie)
+    }
+}
+
+class TheSocialNetwork {
+    private val users = mutableMapOf<Username, User>()
+
+    fun createUser(username: Username) = users
+        .computeIfAbsent(username) {
+            User(
+                username = username,
+                timeline = MutableTimeline(),
+                network = this
+            )
+        }
+
+    fun find(username: Username) = users[username]
 }
 
 @JvmInline
@@ -58,11 +96,19 @@ value class Message(val value: String) {
     init {
         require(value.isNotBlank())
     }
+
+    fun maybeMentions() = "@([a-zA-Z\\d]+)"
+        .toRegex()
+        .find(value)
+        ?.groupValues
+        ?.get(1)
+        ?.let(::Username)
 }
 
 data class TimelineMessage(
     val user: User,
-    val message: Message
+    val message: Message,
+    val mentions: List<User> = emptyList()
 )
 
 interface Timeline {
@@ -85,10 +131,17 @@ data class ImmutableTimeline(val message: List<TimelineMessage>) : Timeline {
 
 data class User(
     val username: Username,
-    val timeline: MutableTimeline
+    val timeline: MutableTimeline,
+    val network: TheSocialNetwork
 ) {
     fun post(message: Message) = apply {
-        timeline.post(TimelineMessage(this, message))
+        timeline.post(
+            TimelineMessage(
+                user = this,
+                message = message,
+                mentions = message.maybeMentions()?.let(network::find)?.let(::listOf).orEmpty()
+            )
+        )
     }
 
     fun viewTimelineFrom(other: User) = ImmutableTimeline(other.timeline.allMessages())
